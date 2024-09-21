4d9ef4f194dc80403c94904ebac728d51b08bb0a
Alexandervkj <alexandeer_junior@hotmail.com>
2024-09-21 13:51:36 -0300

    Criação do app Agendamento

--- agendamento/models.py
+++ agendamento/models.py
@@ -1,3 +1,23 @@
+# models.py
 from django.db import models
 
-# Create your models here.
+class Profissional(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class Agendamento(models.Model):
+    nome = models.CharField(max_length=100)
+    servico = models.CharField(max_length=100)
+    profissional = models.ForeignKey(Profissional, on_delete=models.CASCADE)
+    data = models.DateField()
+    horario = models.TimeField()
+    telefone = models.CharField(max_length=15)
+    confirmacao = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('profissional', 'data', 'horario')
+
+    def __str__(self):
+        return f"{self.nome} - {self.servico} com {self.profissional} em {self.data} às {self.horario}"
